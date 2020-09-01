29a4e6a58757ac7c33f8283b54442a0af6784674
Konstantin Lipkin <konstantinlipkin@localhost.localdomain>
2020-09-01 15:11:40 +0300

    add new model realization

--- training/neural_style_transfer/model.py
+++ training/neural_style_transfer/model.py
@@ -70,45 +70,63 @@ class ConvTanhLayer(nn.Module):
 
     def forward(self, x):
         return self.layers(x)
+        
+class ConvNoTanhLayer(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride):
+        super().__init__()
+        self.layers = nn.Sequential(
+            ConvLayer(in_channels, out_channels, kernel_size, stride),
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
 
 
 class Encoder(nn.Module):
-    def __init__(self, frn=False):
+    def __init__(self, a, b, frn=False):
         super().__init__()
+        filter_counts = list(map(lambda x: int(a * x), [
+            32, 48, 64
+        ]))
         self.layers = nn.Sequential(
-            ConvNormLayer(3, 48, 9, 1, frn=frn),
-            ConvNormLayer(48, 96, 3, 2, frn=frn),
-            ConvNormLayer(96, 192, 3, 2, frn=frn),
-            ResLayer(192, 192, 3, frn=frn),
-            ResLayer(192, 192, 3, frn=frn),
-            ResLayer(192, 192, 3, frn=frn),
-            ResLayer(192, 192, 3, frn=frn)
+            ConvNormLayer(3, filter_counts[0], 3, 1, frn=frn),
+            ConvNormLayer(filter_counts[0], filter_counts[1], 3, 2, frn=frn),
+            ConvNormLayer(filter_counts[1], filter_counts[2], 3, 2, frn=frn),
         )
+        res_layer_count = int(b * 4)
+        [self.layers.add_module(f'{i + 3}', ResLayer(filter_counts[2], filter_counts[2], 3, frn=frn)) 
+        for i in range(res_layer_count)]
+
 
     def forward(self, x):
         return self.layers(x)
 
 
 class Decoder(nn.Module):
-    def __init__(self, frn=False):
+    def __init__(self, a, b, frn=False):
         super().__init__()
+        filter_counts = list(map(lambda x: int(a * x), [
+            64, 48, 32
+        ]))
         self.layers = nn.Sequential(
             nn.Upsample(scale_factor=2),
-            ConvNormLayer(192, 96, 3, 1, frn=frn),
+            ConvNormLayer(filter_counts[0], filter_counts[1], 3, 1, frn=frn),
             nn.Upsample(scale_factor=2),
-            ConvNormLayer(96, 48, 3, 1, frn=frn),
-            ConvTanhLayer(48, 3, 9, 1)
+            ConvNormLayer(filter_counts[1], filter_counts[2], 3, 1, frn=frn),
+            ConvNoTanhLayer(filter_counts[2], 3, 3, 1)
         )
 
     def forward(self, x):
         return self.layers(x)
 
 
-class ReCoNet(nn.Module):
-    def __init__(self, frn=False):
+class ReCoNetMobile(nn.Module):
+    def __init__(self, frn=True, a=0.5, b=0.75):
         super().__init__()
-        self.encoder = Encoder(frn=frn)
-        self.decoder = Decoder(frn=frn)
+        self.encoder = Encoder(a=a, b=b, frn=frn)
+        self.decoder = Decoder(a=a, b=b, frn=frn)
+
 
     def forward(self, x):
         x = self.encoder(x)
