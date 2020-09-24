3d73d67e297f6f8018b7a5d1fe295a63ca8f2e10
Konstantin Lipkin <konstantinlipkin@localhost.localdomain>
2020-09-24 15:16:40 +0300

    add batch training, some structure fixes

--- training/neural_style_transfer/train.py
+++ training/neural_style_transfer/train.py
@@ -25,75 +25,88 @@ from utilities import *
 from network import *
 from totaldata import *
 
+def calc_content_loss(img_features, styled_features, alpha):
+    out = L2distance(styled_features[2],
+                                       img_features[2].expand(styled_features[2].shape))
+    out *= alpha / (styled_features[2].shape[1] *
+                 styled_features[2].shape[2] *
+                 styled_features[2].shape[3])
+                 
+    return out
+    
+def calc_style_loss(style_GM, styled_features, STYLE_WEIGHTS, beta):
+    out = 0
+    for i, weight in enumerate(STYLE_WEIGHTS):
+        if i == 0:
+            continue
+        gram_s = style_GM[i]
+        gram_img = gram_matrix(styled_features[i])
+        #!!! below was gram_img1
+        out += float(weight) * L2distance(gram_img, gram_s.expand(
+            gram_img.size()))
+    out *= beta
+    
+    return out
+    
+def calc_reg_loss(styled_img, gamma):
+    out = gamma * \
+        (torch.sum(torch.abs(styled_img[:, :, :, :-1] - styled_img[:, :, :, 1:])) +
+         torch.sum(torch.abs(styled_img[:, :, :-1, :] - styled_img[:, :, 1:, :])))
+         
+    return out
+    
+def adjust_lr(sample_counter, adjust_lr_every, batch_size, optimizer):
+    if (sample_counter + 1) % adjust_lr_every >= 0 \
+    and (sample_counter + 1) % adjust_lr_every < batch_size:  # 500
+        for param in optimizer.param_groups:
+            param['lr'] = max(param['lr'] / 1.2, 1e-4)
+
 
 def train_first_phase(model, dataloader, optimizer, L2distance, Vgg16, style_GM,
-                      STYLE_WEIGHTS, alpha, beta, gamma, epochs, phase, checkpoint_path, device):
+                      STYLE_WEIGHTS, alpha, beta, gamma, epochs, phase, checkpoint_path, device, save_at, adjust_lr_every):
     data_len = len(dataloader)
+    batch_size = dataloader.batch_size
+    sample_counter = 0
     for epoch in range(epochs):
         running_content_loss = 0
         running_style_loss = 0
         running_reg_loss = 0
         pbar = tqdm.tqdm(enumerate(dataloader), total=len(dataloader))
-        for idx, (img2, _) in pbar:
-            img2 = img2.to(device)
+        for idx, (sample, _) in pbar:
+            sample = sample.to(device)
+            sample_counter += batch_size
+            
+            adjust_lr(sample_counter, adjust_lr_every, batch_size, optimizer)
+             
+            losses = []
+            for img in sample:
+                img = img.unsqueeze(0)
+                feature_map, styled_img = model(img)
+                styled_img = normalize_after_reconet(styled_img)
+                img = normalize_after_reconet(img)
+
+                styled_features = Vgg16(styled_img)
+                img_features = Vgg16(img)
+
+                content_loss = calc_content_loss(img_features, styled_features, alpha)
+
+                style_loss = calc_style_loss(style_GM, styled_features, STYLE_WEIGHTS, beta)
+
+                reg_loss = calc_reg_loss(styled_img, gamma)
+                
+                running_content_loss += content_loss.item()
+                running_style_loss += style_loss.item()
+                running_reg_loss += reg_loss.item()
+
+                sample_loss = content_loss + style_loss + reg_loss
+                losses.append(sample_loss)
+                
+            loss = sum(losses) / len(losses)
             optimizer.zero_grad()
-            if (idx + 1) % 8e4 == 0:  # 500
-                for param in optimizer.param_groups:
-                    param['lr'] = max(param['lr'] / 1.2, 1e-4)
-
-            feature_map2, styled_img2 = model(img2)
-            styled_img2 = normalize_after_reconet(styled_img2)
-            img2 = normalize_after_reconet(img2)
-
-            styled_features2 = Vgg16(styled_img2)
-            img_features2 = Vgg16(img2)
-
-            content_loss = 0
-            content_loss += L2distance(styled_features2[2],
-                                       img_features2[2].expand(styled_features2[2].size()))
-            # was styled_features1 below  !!!
-            content_loss *= alpha / \
-                (styled_features2[2].shape[1] *
-                 styled_features2[2].shape[2] *
-                 styled_features2[2].shape[3])
-
-            style_loss = 0
-            for i, weight in enumerate(STYLE_WEIGHTS):
-                if i == 0:
-                    continue
-                print("here")
-                gram_s = style_GM[i]
-                # print(styled_features1[i].size())
-                gram_img2 = gram_matrix(styled_features2[i])
-                # print(gram_img1.size(), gram_s.size())
-                #!!! below was gram_img1
-                style_loss += float(weight) * L2distance(gram_img2, gram_s.expand(
-                    gram_img2.size()))
-            print()
-            style_loss *= beta
-
-            reg_loss = 0
-            reg_loss += gamma * \
-                (torch.sum(torch.abs(styled_img2[:, :, :, :-1] - styled_img2[:, :, :, 1:])) +
-                 torch.sum(torch.abs(styled_img2[:, :, :-1, :] - styled_img2[:, :, 1:, :])))
-
-            # print(f_temporal_loss.size(), o_temporal_loss.size(),
-            # content_loss.size(), style_loss.size(), reg_loss.size())
-            loss = content_loss + style_loss + reg_loss
-            # loss = content_loss + style_loss
             loss.backward()
             optimizer.step()
-            #
-            #
-            # if (idx+1)%1000 ==0 :
-            # torch.save(model.state_dict(), '%s/final_reconet_epoch_%d_idx_%d.pth' %
-            # ("runs/output", epoch, idx//1000))
 
             scale_value = 1 / batch_size / max(idx, 1)
-            count = img2.shape[0]
-            running_content_loss += content_loss.item() * count
-            running_style_loss += style_loss.item() * count
-            running_reg_loss += reg_loss.item() * count
             pbar.set_description(
                 "Epoch: {}/{} Losses -> Content: {:.4f} Style: {:.4f} Reg: {:.4f}".format(
                     epoch,
@@ -103,10 +116,7 @@ def train_first_phase(model, dataloader, optimizer, L2distance, Vgg16, style_GM,
                     running_reg_loss * scale_value
                 )
             )
-            # print('[%d/%d][%d/%d] SL: %.4f CL: %.4f FTL: %.4f OTL: %.4f RL: %.4f'
-            #               % (epoch, epochs, idx, len(dataloader),
-            # style_loss, content_loss , f_temporal_loss, o_temporal_loss, reg_loss))
-            if idx in (int(data_len / 3.), data_len - 1):
+            if checkpoint_path is not None and idx in (int(data_len * save_at) - 1, data_len - 1):
                 torch.save(
                     model.state_dict(),
                     os.path.join(checkpoint_path, 'reconet_phase_{}_epoch_{}_loss_{:.4f}.pth'.format(
@@ -116,7 +126,7 @@ def train_first_phase(model, dataloader, optimizer, L2distance, Vgg16, style_GM,
 
 
 def train_second_phase(model, dataloader, optimizer, L2distance, L2distancematrix, Vgg16, style_GM,
-                       STYLE_WEIGHTS, alpha, beta, gamma, lambda_o, lambda_f, epochs, phase, checkpoint_path, device):
+                       STYLE_WEIGHTS, alpha, beta, gamma, lambda_o, lambda_f, epochs, phase, checkpoint_path, device, save_at, adjust_lr_every):
     for epoch in range(epochs):
         running_content_loss = 0
         running_style_loss = 0
@@ -288,6 +298,10 @@ if __name__ == '__main__':
                         help="Use Filter Response Normalization and TLU")
     parser.add_argument("--use_skip", action='store_true',
                         help="Use skip connections")
+    parser.add_argument("--save_at", default=1,
+                        help="Save checkpoint at current training stage, float in (0, 1)")
+    parser.add_argument("--adjust_lr_every", default=1,
+                        help="Lr decrease factor")
 
     args = parser.parse_args()
     manual_weights = args.manual_weights
@@ -309,12 +323,14 @@ if __name__ == '__main__':
     style_path = args.style_path
     checkpoint_path = args.checkpoint_path
     model_path = args.model_path
-    batch_size = args.batch_size
+    batch_size = int(args.batch_size)
     phase = args.phase
     epochs = args.epochs
     lr = args.lr
     frn = args.frn
     use_skip = args.use_skip
+    save_at = args.save_at
+    adjust_lr_every = args.adjust_lr_every
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     # dataloader = DataLoader(FlyingChairsDataset("../FlyingChairs2/"),
@@ -322,7 +338,7 @@ if __name__ == '__main__':
     if phase == 'first':
         IMG_SIZE = (400, 400) # 256, 256
         transform = T.Compose([
-#            T.Resize(IMG_SIZE), # no resize if image were resized
+            T.Resize(IMG_SIZE), # no resize if image were resized
             T.RandomHorizontalFlip(),
             T.ToTensor(),
             T.Lambda(lambda x: x.mul(2).sub(1))
@@ -386,7 +402,7 @@ if __name__ == '__main__':
 
     if phase == 'first':
         train_first_phase(model, dataloader, optimizer, L2distance, Vgg16, style_GM,
-                          STYLE_WEIGHTS, alpha, beta, gamma, epochs, phase, checkpoint_path, device)
+                          STYLE_WEIGHTS, alpha, beta, gamma, epochs, phase, checkpoint_path, device, save_at, adjust_lr_every)
     if phase == 'second':
         train_second_phase(model, dataloader, optimizer, L2distance, L2distancematrix, Vgg16, style_GM,
-                           STYLE_WEIGHTS, alpha, beta, gamma, lambda_o, lambda_f, epochs, phase, checkpoint_path, device)
+                           STYLE_WEIGHTS, alpha, beta, gamma, lambda_o, lambda_f, epochs, phase, checkpoint_path, device, save_at, adjust_lr_every)
